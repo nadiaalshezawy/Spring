8161b367a3d3906db5c53f15ac721418c3ad1eb0
Nadia Ahmed <nadiaalshezawy@gmail.com>
2020-10-08 10:53:34 +0400

    need to fix problem with version

--- demo/demo/src/main/java/com/example/demo/api/PersonController.java
+++ demo/demo/src/main/java/com/example/demo/api/PersonController.java
@@ -3,8 +3,10 @@ package com.example.demo.api;
 import com.example.demo.model.Person;
 import com.example.demo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.UUID;
 
@@ -18,7 +20,7 @@ public class PersonController {
         this.personService = personService;
     }
 @PostMapping
-    public void addPerson(@RequestBody Person person)
+    public void addPerson(@Valid @NonNull @RequestBody Person person)
     {
         personService.addPerson(person);
     }
@@ -31,5 +33,15 @@ public class PersonController {
     public Person getPersonById(@PathVariable("id")UUID id){
          return personService.getPersonById(id)
                  .orElse(null);
+    }
+
+    @DeleteMapping(path ="{id}")
+    public void deletePersonById(@PathVariable("id") UUID id){
+        personService.deletePerson(id);
+    }
+
+    @PutMapping(path ="{id}")
+    public void updatePerson(@PathVariable("id") UUID id,@Valid @NonNull @RequestBody Person personToUpdate){
+        personService.updatePerson(id, personToUpdate);
     }
 }
